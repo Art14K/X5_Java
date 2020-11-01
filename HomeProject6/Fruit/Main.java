5bb7c99b61c9064da84afec8511114b9ffed6ff9
artem <volot14@gmail.com>
2020-11-01 11:46:48 +0300

    Домашнее задание 6 (Фрукты)

--- HomeProject6/Fruit/Main.java
+++ HomeProject6/Fruit/Main.java
@@ -3,26 +3,26 @@ import java.util.*;
 public class Main {
     public static void main (String[] args) {
 
-        Box<Orange> or = new Box<>();
-        Box<Orange> or1 = new Box<>();
-        Box<Apple> ap = new Box<>();
-        Box<Apple> ap1 = new Box<>();
-        or.addFruit(new Orange(),14);
-        or1.addFruit(new Orange(),18);
-        ap.addFruit(new Apple(),12);
-        ap1.addFruit(new Apple(),15);
-        System.out.println("Коробка 1: "+or.getWeight());
-        System.out.println("Коробка 2: "+or1.getWeight());
-        System.out.println("Коробка 3: "+ap.getWeight());
-        System.out.println("Коробка 4: "+ap1.getWeight());
-        System.out.println("Сравниваем коробку 1 с коробкой 3: "+or.compare(ap));
-        System.out.println("Сравниваем коробку 2 с коробкой 4: "+or1.compare(ap1));
-        or.pourTo(or1);
-        ap.pourTo(ap1);
-        System.out.println("Коробка 1: "+or.getWeight());
-        System.out.println("Коробка 2: "+or1.getWeight());
-        System.out.println("Коробка 3: "+ap.getWeight());
-        System.out.println("Коробка 4: "+ap1.getWeight());
+        Box<Orange> orange = new Box<>();
+        Box<Orange> orange_1 = new Box<>();
+        Box<Apple> apple = new Box<>();
+        Box<Apple> apple_1 = new Box<>();
+        orange.addFruit(new Orange(),14);
+        orange_1.addFruit(new Orange(),18);
+        apple.addFruit(new Apple(),12);
+        apple_1.addFruit(new Apple(),15);
+        System.out.println("Коробка 1: "+orange.getWeight());
+        System.out.println("Коробка 2: "+orange_1.getWeight());
+        System.out.println("Коробка 3: "+apple.getWeight());
+        System.out.println("Коробка 4: "+apple_1.getWeight());
+        System.out.println("Сравниваем коробку 1 с коробкой 3: "+orange.compare(apple));
+        System.out.println("Сравниваем коробку 2 с коробкой 4: "+orange_1.compare(apple_1));
+        orange.pourTo(orange_1);
+        apple.pourTo(apple_1);
+        System.out.println("Коробка 1: "+orange.getWeight());
+        System.out.println("Коробка 2: "+orange_1.getWeight());
+        System.out.println("Коробка 3: "+apple.getWeight());
+        System.out.println("Коробка 4: "+apple_1.getWeight());
 
 
     }
