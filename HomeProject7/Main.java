9de803e06cef33a92b778bc2bf60b6e55a8604d1
artem <volot14@gmail.com>
2020-11-03 18:20:35 +0300

    Домашнее задание 7

--- HomeProject7/Main.java
+++ HomeProject7/Main.java
@@ -5,19 +5,22 @@ public class Main {
     public static void main(String[] args) {
         int start = 1;
         Scanner scan = new Scanner(System.in);
+        String input;
+        String[] Input = new String[5];
         while (start > 0) {
             try {
-                switch (args[0]) {
+                System.out.print("Введите команду(для выхода из программы введите exit: ");
+                input = scan.nextLine();
+                Input = input.split(" ");
+                switch (Input[0]) {
                     case "exit":
                         start = 0;
-                    default:
-                        args[0] = scan.nextLine();
-                }
+
+                   }
             } catch (ArrayIndexOutOfBoundsException exc) {
                 System.out.println("Ошибка ввода!");
-            } finally {
-                args[0] = scan.nextLine();
             }
+                 
         }
     }
 }
